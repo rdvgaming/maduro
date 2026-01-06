0a1a0492baeac349165323b97acbe182c14f9eb2
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2026-01-06 19:53:05 +0100

    Reorganize game code into standalone directory
    - Move all game source files to src/games/maduro-survivor/
    - Move game assets to src/games/maduro-survivor/assets/
    - Add esbuild for bundling the game
    - Update assets.ts to properly import images
    - Add build:game script to bundle game before site build
    - Regenerate game bundle with proper module imports
    
    All file moves preserve git history using git mv

--- src/assets.ts
+++
@@ -1,4 +0,0 @@
-// Image assets as data URIs
-export const MADURO_IMAGE = "data:image/png;base64,$(cat maduro_base64.txt)";
-export const MARINE_IMAGE = "data:image/png;base64,$(base64 -i marine.png | tr -d '\n')";
-export const TRUMP_IMAGE = "data:image/png;base64,$(base64 -i trump.png | tr -d '\n')";
---
+++ src/games/maduro-survivor/assets.ts
@@ -0,0 +1,8 @@
+// Image assets
+import maduroImage from "./assets/maduro.png";
+import marineImage from "./assets/marine.png";
+import trumpImage from "./assets/trump.png";
+
+export const MADURO_IMAGE = maduroImage;
+export const MARINE_IMAGE = marineImage;
+export const TRUMP_IMAGE = trumpImage;
