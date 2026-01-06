74615102b5fb1ff4757233443a9299b675a5f9ba
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2026-01-06 20:50:49 +0100

    Use relative paths for asset imports

--- src/lib/maduro-survivors/enemy-types.ts
+++ src/lib/maduro-survivors/enemy-types.ts
@@ -1,5 +1,5 @@
 import { Enemy } from "./types";
-import trumpImgModule from "/maduro/assets/maduro-survivors/trump.png";
+import trumpImgModule from "../../../public/maduro/assets/maduro-survivors/trump.png";
 
 export class Trump extends Enemy {
   static trumpSprite: HTMLImageElement | null = null;
